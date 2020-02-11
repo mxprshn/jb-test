c145b1c00364533f6b864219fcd92c5ae70159ad
Max Parshin <43303110+mxprshn@users.noreply.github.com>
2020-02-11 22:50:09 +0300

    Added signing-in to google

--- app/src/main/java/com/example/jbtest/MainActivity.kt
+++ app/src/main/java/com/example/jbtest/MainActivity.kt
@@ -1,12 +1,57 @@
 package com.example.jbtest
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.Scope
+import com.google.android.gms.tasks.Task
+import com.google.api.services.drive.DriveScopes
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mGoogleSignInClient : GoogleSignInClient
+
+    companion object {
+        private const val REQUEST_SIGN_IN = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val signInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestScopes(Scope(DriveScopes.DRIVE_FILE))
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, signInOptions)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+
+        if (account == null) {
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, REQUEST_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_SIGN_IN) {
+            GoogleSignIn.getSignedInAccountFromIntent(data)
+        }
     }
 }
